de3d320943abece8847be1cac33a615600b250a6
Vladislav Radkevich <r.uladzislau.m@gmail.com>
2025-07-13 23:30:42 +0200

    Test Jenkins

--- src/test/java/com/vladislav/onlinertest/models/pages/ProductPricesPage.java
+++ src/test/java/com/vladislav/onlinertest/models/pages/ProductPricesPage.java
@@ -1,42 +1,37 @@
 package com.vladislav.onlinertest.models.pages;
 
 import com.vladislav.onlinertest.core.element.Element;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
-
+import io.qameta.allure.Step;
 
 
 public class ProductPricesPage {
     private final Element sortByDropdown = Element.byCss(".input-style__real");
-    private final Element cart = Element.byXpath("((//div[contains(@class, 'offers-list__item')])[1]//a[contains(@class, 'button-style') and contains(text(), 'В корзину')])[2]");
-    private final Element toCart = Element.byXpath("(//div[contains(@class, 'offers-list__item')])[1]//a[normalize-space()='Перейти в корзину']");
+    private final Element cart = Element.byXpath("((//div[contains(@class, 'offers-list__item')])[1]//a[contains(@class, 'offers-list__button_cart') and normalize-space()='В корзину'])[2]");
+    private final Element toCart = Element.byXpath("  //a[contains(@class, 'button-style button-style_another button-style_base-')]");
 
-    private final WebDriver driver;
+    private final Element sort = Element.byXpath("//div[@class='input-style__faux']");
+    private final Element sortCheck = Element.byXpath("//div[@class = 'offers-filter__part offers-filter__part_2']");
 
-    public ProductPricesPage(WebDriver driver) {
-        this.driver = driver;
-    }
-
-
-    public void selectSortByValue(String value) {
-        WebElement dropdown = sortByDropdown.getElement();
-        Select select = new Select(dropdown);
-        select.selectByValue(value);
-        sortByDropdown.dropDownMenuClick(dropdown);
-    }
-
+    @Step("Sort products by price ascending")
     public void sortByPriceAscending() {
-
-        selectSortByValue("price:asc");
+        sortByDropdown.selectSortByValue("price:asc");
     }
 
+    @Step("Add product to cart")
     public void addProductToCart() {
         sortByPriceAscending();
-        cart.waitClickableJS();
 
+
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        cart.waitClickableJS();
     }
 
+    @Step("Open cart page")
     public  void openCart() {
         toCart.waitClickableClick();
     }
--- src/test/java/com/vladislav/onlinertest/models/pages/SearchResultsPage.java
+++ src/test/java/com/vladislav/onlinertest/models/pages/SearchResultsPage.java
@@ -1,86 +1,26 @@
 package com.vladislav.onlinertest.models.pages;
 
 import com.vladislav.onlinertest.core.element.Element;
-import com.vladislav.onlinertest.models.Product;
-import com.vladislav.onlinertest.utils.ProductParser;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebElement;
 
-import java.util.Comparator;
-import java.util.List;
-
 public class SearchResultsPage {
-    private final ProductParser productParser;
-    private final Element productLocator =  Element.byXpath("//div[contains(@class, 'result__item_product')]");
+
+    private final Element productLocator = Element.byXpath("//div[contains(@class, 'result__item_product')]");
     private final Element priceButtonLocator = Element.byXpath("//*[@id=\"search-page\"]/div[2]/ul/li[1]/div/div/div[1]/div/a");
+    private final Element titleElement = Element.byCss(".product__title-link");
 
 
-    public SearchResultsPage() {
-        this.productParser = new ProductParser();
+    @Step("Get actual product name")
+    public String getActualProductName() {
+        return productLocator.getNestedElementByLocator(titleElement.getBy()).getText();
     }
 
-
-    public List<Product> getProducts() {
-
-        List<WebElement> productElements = productLocator.getElements();
-        return productParser.parseProducts(productElements);
+    @Step("Open product prices page ")
+    public ProductPricesPage openProductPricesPage(String name) {
+        WebElement element = productLocator.getProductCardByName(name);
+        priceButtonLocator.clickNested(element);
+        return new ProductPricesPage();
     }
-
-    public Product getCheapestProduct() {
-        List<Product> products = getProducts();
-        return products.stream()
-                .min(Comparator.comparing(Product::getPrice))
-                .orElseThrow(() -> new RuntimeException("Failed to find the product with the minimum price."));
-    }
-
-    public Product getProductByName(String productName) {
-        List<Product> products = getProducts();
-
-        return products.stream()
-                .filter(product -> product.getName().equalsIgnoreCase(productName))
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("Product with name '" + productName + "' not found."));
-    }
-
-
-    private WebElement getProductCardByName(String productName) {
-
-        List<WebElement> cards = productLocator.getElements();
-
-        for (WebElement card : cards) {
-            try {
-                WebElement titleElement = card.findElement(By.cssSelector(".product__title-link"));
-                String actualTitle = titleElement.getText();
-
-                if (actualTitle.equalsIgnoreCase(productName)) {
-                    return card;
-
-                }
-            } catch (NoSuchElementException e) {
-
-            }
-        }
-        throw new NoSuchElementException("Product with name '" + productName + "' not found in search results.");
-    }
-
-
-
-    public String getActualProductName(String productName) {
-
-        WebElement productCard = getProductCardByName(productName);
-
-        WebElement titleElement = productCard.findElement(By.cssSelector(".product__title-link"));
-
-        return titleElement.getText();
-    }
-
-    public ProductPricesPage openProductPricesPage(String name, WebDriver driver ) {
-        WebElement element = getProductCardByName(name);
-       priceButtonLocator.clickNested(element);
-     return new  ProductPricesPage(driver);
-    }
-
-
 }
