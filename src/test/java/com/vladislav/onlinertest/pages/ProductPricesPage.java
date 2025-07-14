ca21782f13ad7affa9707c7e2c435bd881e9ebf7
Vladislav Radkevich <r.uladzislau.m@gmail.com>
2025-07-14 20:15:21 +0200

    Test Jenkins

--- src/test/java/com/vladislav/onlinertest/pages/ProductPricesPage.java
+++ src/test/java/com/vladislav/onlinertest/pages/ProductPricesPage.java
@@ -7,7 +7,7 @@ import io.qameta.allure.Step;
 public class ProductPricesPage {
     private final Element sortByDropdown = Element.byCss(".input-style__real");
     private final Element cart = Element.byXpath("((//div[contains(@class, 'offers-list__item')])[1]//a[contains(@class, 'offers-list__button_cart') and normalize-space()='В корзину'])[2]");
-    private final Element toCart = Element.byXpath("  //a[contains(@class, 'button-style button-style_another button-style_base-')]");
+    private final Element toCart = Element.byXpath("//a[contains(@class, 'button-style button-style_another button-style_base-')]");
 
     private final Element sort = Element.byXpath("//div[@class='input-style__faux']");
     private final Element sortCheck = Element.byXpath("//div[@class = 'offers-filter__part offers-filter__part_2']");
--- src/test/java/com/vladislav/onlinertest/test/MainPageTest.java
+++ src/test/java/com/vladislav/onlinertest/test/MainPageTest.java
@@ -1,11 +1,11 @@
 package com.vladislav.onlinertest.test;
 
-
 import com.vladislav.onlinertest.core.driver.WebDriverSingleton;
-import com.vladislav.onlinertest.models.pages.MainPage;
-import com.vladislav.onlinertest.models.pages.SearchResultsPage;
+import com.vladislav.onlinertest.pages.MainPage;
+
 import com.vladislav.onlinertest.pages.CartPage;
 import com.vladislav.onlinertest.pages.ProductPricesPage;
+import com.vladislav.onlinertest.pages.SearchResultsPage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
