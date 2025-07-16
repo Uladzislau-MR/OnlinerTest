45be67ed068efeae93e69c9ea071557abd0e7fa9
Vladislav Radkevich <r.uladzislau.m@gmail.com>
2025-07-16 09:13:16 +0200

    Test Jenkins

--- src/test/java/com/vladislav/onlinertest/suite/NegativeTestSuite.java
+++ src/test/java/com/vladislav/onlinertest/suite/NegativeTestSuite.java
@@ -1,11 +1,10 @@
 package com.vladislav.onlinertest.suite;
 
-import com.vladislav.onlinertest.test.MainPageTest;
 import com.vladislav.onlinertest.test.NegativeTest;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectClasses({NegativeTest.class})
-public class SmokeTestSuite {
+public class NegativeTestSuite {
 }
