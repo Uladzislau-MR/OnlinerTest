3c128346c3e50839eb766266197edffafed67f3c
Vladislav Radkevich <r.uladzislau.m@gmail.com>
2025-07-16 13:56:42 +0200

    Test Jenkins

--- src/test/java/com/vladislav/onlinertest/core/driver/WebDriverFactory.java
+++ src/test/java/com/vladislav/onlinertest/core/driver/WebDriverFactory.java
@@ -1,22 +1,22 @@
 package com.vladislav.onlinertest.core.driver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 import java.util.Collections;
-
 
 @Log4j2
 public class WebDriverFactory {
@@ -26,7 +26,6 @@ public class WebDriverFactory {
 
     public static WebDriver getDriver() {
         WebDriver driver;
-
         String browser = System.getProperty("browser");
         if (browser == null) {
             log.warn("Browser property is not set. Defaulting to 'chrome'.");
@@ -39,8 +38,15 @@ public class WebDriverFactory {
                 if (HOST != null) {
                     driver = createRemoteDriver(options);
                 } else {
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver(options);
+                    System.setProperty("webdriver.chrome.verboseLogging", "true");
+
+                    File logFile = new File("chromedriver.log");
+                    ChromeDriverService service = new ChromeDriverService.Builder()
+                            .withLogFile(logFile)
+                            .build();
+
+                    log.info("ChromeDriver log file will be created at: " + logFile.getAbsolutePath());
+                    driver = new ChromeDriver(service, options);
                 }
                 break;
             }
@@ -49,7 +55,6 @@ public class WebDriverFactory {
                 if (HOST != null) {
                     driver = createRemoteDriver(options);
                 } else {
-                    WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver(options);
                 }
                 break;
@@ -60,7 +65,6 @@ public class WebDriverFactory {
 
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.manage().window().maximize();
-
         log.info("Created WebDriver instance " + driver.getClass().getSimpleName());
         return driver;
     }
@@ -74,46 +78,33 @@ public class WebDriverFactory {
         }
     }
 
-
     private static FirefoxOptions getFirefoxOptions() {
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         FirefoxProfile profile = new FirefoxProfile();
-
-        // Settings for managing file downloads
-        profile.setPreference("browser.download.folderList", 2); // 2 means download to the specified directory
+        profile.setPreference("browser.download.folderList", 2);
         profile.setPreference("browser.download.dir", DOWNLOADS_PATH);
-        profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream");  // File types for automatic download
-
-        // Settings to mask automation detection
+        profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream");
         profile.setPreference("dom.webdriver.enabled", false);
         profile.setPreference("devtools.chrome.enabled", false);
         profile.setPreference("useAutomationExtension", false);
-
         firefoxOptions.setProfile(profile);
         return firefoxOptions;
     }
 
     private static ChromeOptions getChromeOptions() {
         ChromeOptions options = new ChromeOptions();
-
         options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-
         options.addArguments("--headless");
         options.addArguments("--disable-gpu");
         options.addArguments("--disable-software-rasterizer");
         options.addArguments("--disable-features=VizDisplayCompositor");
-
         options.addArguments("--disable-extensions");
         options.addArguments("--disable-browser-side-navigation");
         options.addArguments("--dns-prefetch-disable");
-
         options.addArguments("--window-size=1920,1080");
-
         options.addArguments("--user-data-dir=" + FileUtils.getTempDirectoryPath() + "chrome-profile-" + System.currentTimeMillis());
-
         return options;
     }
 }
